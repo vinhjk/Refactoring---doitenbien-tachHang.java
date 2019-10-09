0b48143d449bb578833b641459c104e7d5ba8d51
NguyenVinh <nguyenquangvinh2518@gmail.com>
2019-10-09 16:58:52 +0700

    refactor

---
+++ src/refactor.java
@@ -0,0 +1,28 @@
+public class refactor {
+    public static final char ADDITION = '+';
+    public static final char SUBTRACTION = '-';
+    public static final char MULTIPLICATION = '*';
+    public static final char DIVISION = '/';
+
+    public static int calculate(int firstOperand, int secondOperand, char operator) {
+        switch (operator) {
+            case ADDITION:
+                return firstOperand + secondOperand;
+            case SUBTRACTION:
+                return firstOperand - secondOperand;
+            case MULTIPLICATION:
+                return firstOperand * secondOperand;
+            case DIVISION:
+                if (secondOperand != 0)
+                    return firstOperand / secondOperand;
+                else
+                    throw new RuntimeException("Can not divide by 0");
+            default:
+                throw new RuntimeException("Unsupported operation");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculate(3,4,ADDITION));
+    }
+}
